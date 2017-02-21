6267617d644e7349c8c07d687bfee6cdfef1855d
ReeceLangerock <reecelangerock@gmail.com>
2017-02-20 18:39:38 -0600

    fixed?

--- bot.js
+++ bot.js
@@ -10,16 +10,18 @@ function respond() {
 
   if(request.text && scheduleRegex.test(request.text)) {
     var team = scheduleRegex.exec(request.text);
+    console.log(team);
     var url = "http://daddyleagues.com/"+process.env.LEAGUE_NAME+"/team/"+team+"/schedule";
-    this.res.writeHead(200);
+    //this.res.writeHead(200);
     postResponse(url);
-    this.res.end();
+    //this.res.end();
   } else if(request.text && rosterRegex.test(request.text)){
+    console.log(team);
     var team = rosterRegex.exec(request.text);
     var url = "http://daddyleagues.com/"+process.env.LEAGUE_NAME+"/team/"+team+"/roster";
-    this.res.writeHead(200);
+    //this.res.writeHead(200);
     postResponse(url);
-    this.res.end();
+    //this.res.end();
   }
   //already have bot doing requests to /dl
   /*else if(request.text && dlRegex.test(request.text)){
